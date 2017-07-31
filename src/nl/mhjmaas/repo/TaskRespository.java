402fed0245fe079cc7eb821f7f196068fcc1b843
Marcel Maas <maas.marcel@gmail.com>
2017-07-31 11:43:31 +0200

    Initial commit of the tasks api

---
+++ src/nl/mhjmaas/repo/TaskRespository.java
@@ -0,0 +1,10 @@
+package nl.mhjmaas.repo;
+
+import nl.mhjmaas.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TaskRespository extends MongoRepository<Task, String> {
+    public List<Task> findByDescription(String description);
+}
